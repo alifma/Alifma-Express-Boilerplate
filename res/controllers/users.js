216ffd619b2ccb1d99796e08b8efc68a24ef9755
alifma <alifmaulana26@gmail.com>
2021-03-14 18:44:01 +0700

    Update User & Email Duplicate Checker

--- res/controllers/users.js
+++ res/controllers/users.js
@@ -1,22 +1,32 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const moment = require('moment');
-const { error, success } = require('../helpers/response')
-const { mCheckEmail, mRegister, mDetail, mSendMail, mUpdateUser } = require('../models/users')
+const {
+  error,
+  success
+} = require('../helpers/response')
+const {
+  mCheckEmail,
+  mRegister,
+  mDetail,
+  mSendMail,
+  mUpdateUser
+} = require('../models/users')
+const fs = require('fs')
 module.exports = {
-  cRegister : async (req, res) => {
+  cRegister: async (req, res) => {
     const body = req.body
     try {
-      if (body.email && body.password && body.username ) {
+      if (body.email && body.password && body.username) {
         mCheckEmail(body.email)
-          .then(async (response)=>{
-            if(response.length >= 1) {
+          .then(async (response) => {
+            if (response.length >= 1) {
               if (response[0].active === 0) {
                 error(res, 400, 'Registration Failed', 'Email already registered and need activation')
               } else {
                 error(res, 400, 'Registration Failed', 'Email already registered, Please login')
               }
-            }else{
+            } else {
               const salt = await bcrypt.genSalt(10)
               const password = await bcrypt.hash(body.password, salt)
               const user = {
@@ -26,65 +36,68 @@ module.exports = {
                 password
               }
               mRegister(user)
-              .then(async (resRegist)=> {
-                const dataUser = {
-                  email: body.email,
-                  id: resRegist.insertId
-                }
-                const jwttoken = jwt.sign(dataUser, process.env.JWT_SECRET)
-                const link = `${process.env.IP_SERVER}:${process.env.PORT}/api/verify/${jwttoken}`
-                const mailData =  `
+                .then(async (resRegist) => {
+                  const dataUser = {
+                    email: body.email,
+                    id: resRegist.insertId
+                  }
+                  const jwttoken = jwt.sign(dataUser, process.env.JWT_SECRET)
+                  const link = `${process.env.IP_SERVER}:${process.env.PORT}/api/verify/${jwttoken}`
+                  const mailData = `
                 <div style="text-align:center">
                 <h1>Please Confirm Your Account</h1>
                 <p> Click the link below to confirm you account</p>
                 <a href="${link}">Verif your account</a>
                 </div>
                 `
-                mSendMail(body.email, "Account Confirmation", mailData)
-                  .then((resVerif) => {
-                    success(res, 200, 'Registration Success, Check your email to verif your account!', {})
-                  })
-                  .catch((err)=> error(res, 400, 'Registration Failed', err.message))
-              })
-              .catch((err)=> error(res, 400, 'Registration Failed', err.message))
+                  mSendMail(body.email, "Account Confirmation", mailData)
+                    .then((resVerif) => {
+                      success(res, 200, 'Registration Success, Check your email to verif your account!', {})
+                    })
+                    .catch((err) => error(res, 400, 'Registration Failed', err.message))
+                })
+                .catch((err) => error(res, 400, 'Registration Failed', err.message))
             }
           })
-          .catch((err)=> error(res, 400, 'Registration Failed', err.message))
+          .catch((err) => error(res, 400, 'Registration Failed', err.message))
       } else {
-        error (res, 400, 'Empty Field Found', 'Please Fill All Data') 
+        error(res, 400, 'Empty Field Found', 'Please Fill All Data')
       }
     } catch {
       error(res, 500, 'Internal Server Error', '')
     }
   },
   // Login
-  cLogin : (req, res) => {
+  cLogin: (req, res) => {
     const body = req.body
     try {
       if (body.email && body.password) {
         mCheckEmail(body.email)
-        .then(async (response)=>{
-          if(response.length == 1){
-            const checkPassword = await bcrypt.compare(body.password, response[0].password)
-            if(checkPassword) {
-              if (response[0].active === 1) {
-                const dataUser = {
-                  email: response[0].email,
-                  id: response[0].id
+          .then(async (response) => {
+            if (response.length == 1) {
+              const checkPassword = await bcrypt.compare(body.password, response[0].password)
+              if (checkPassword) {
+                if (response[0].active === 1) {
+                  const dataUser = {
+                    email: response[0].email,
+                    id: response[0].id
+                  }
+                  const jwttoken = jwt.sign(dataUser, process.env.JWT_SECRET)
+                  success(res, 200, 'Login Success', {}, {
+                    user: response[0],
+                    token: jwttoken
+                  })
+                } else {
+                  error(res, 400, 'Login Failed', 'User not Activated, Please Check Your email')
                 }
-                const jwttoken = jwt.sign(dataUser, process.env.JWT_SECRET)
-                success(res, 200, 'Login Success', {}, {user: response[0], token: jwttoken})
               } else {
-                error(res, 400, 'Login Failed', 'User not Activated, Please Check Your email')
+                error(res, 400, 'Login Failed', 'Wrong Password')
               }
-            }else{  
-              error(res, 400, 'Login Failed', 'Wrong Password')
+            } else {
+              error(res, 400, 'Login Failed', 'Email is not registered')
             }
-          }else{
-            error(res, 400, 'Login Failed', 'Email is not registered')
-          }
-        })
-        .catch((err)=> error(res, 400, 'Login Failed', err.message))
+          })
+          .catch((err) => error(res, 400, 'Login Failed', err.message))
       } else {
         error(res, 500, 'Empty Field Found', 'Please Fill All Data')
       }
@@ -92,30 +105,123 @@ module.exports = {
       error(res, 500, 'Internal Server Error', '')
     }
   },
-  cDetail : (req, res) => {
+  cDetail: (req, res) => {
     const id = req.params.id
     mDetail(id)
       .then((response) => {
-        success(res, 200, 'Display Detail User Success', {},  response)
+        success(res, 200, 'Display Detail User Success', {}, response)
       })
       .catch((err) => {
         error(res, 500, 'Internal Server Error', err.message, {})
       })
   },
-  cActivate: async(req, res) => {
+  // Update User
+  cUpdateUser: async (req, res) => {
+    try {
+      const id = req.params.id
+      const data = req.body
+      // Semua kolom yang bisa diperbaharui dari Database
+      if (req.file || data.username || data.first_name || data.last_name || data.email || data.password) {
+        let dataUpdate = {}
+        // Kalau ada file, maka Imagenya dibawa
+        if (req.file) {
+          dataUpdate = {
+            ...data,
+            image: req.file.filename
+          }
+          mDetailUser(id)
+            .then((res) => {
+              if (res[0].image !== 'userdefault.png') {
+                fs.unlinkSync(`./public/img/${res[0].image}`)
+              }
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+        } else {
+          dataUpdate = {
+            ...data
+          }
+        }
+        if (data.email) {
+          await mCheckEmail(data.email)
+            .then(async (resCheck) => {
+              await mDetail(id)
+                .then(async (resDetail) => {
+                  if (resCheck.length >= 1 && (resCheck[0].email != resDetail[0].email)) {
+                    // Kalau email sudah dipakai sama yang lain
+                    if (req.file) {
+                      fs.unlinkSync(`./public/img/${req.file.filename}`)
+                    }
+                    error(res, 400, 'Email already Used!', 'Please use your old email or another email!', {})
+                  } else {
+                    // Kalau emailnya pakai email lama atau pakai email baru
+                    await mUpdateUser(dataUpdate, id)
+                      .then((response) => {
+                        if (response.affectedRows != 0) {
+                          success(res, 200, 'Update data Success', {}, {})
+                        } else {
+                          if (req.file) {
+                            fs.unlinkSync(`./public/img/${req.file.filename}`)
+                          }
+                          error(res, 400, 'Nothing Patched, Wrong ID', {}, {})
+                        }
+                      })
+                      .catch((err) => {
+                        if (req.file) {
+                          fs.unlinkSync(`./public/img/${req.file.filename}`)
+                        }
+                        error(res, 400, 'Wrong Data Type Given', err.message, {})
+                      })
+                  }
+                })
+            })
+        } else {
+          mUpdateUser(dataUpdate, id)
+            .then((response) => {
+              if (response.affectedRows != 0) {
+                success(res, 200, 'Patch data Success', {}, {})
+              } else {
+                if (req.file) {
+                  fs.unlinkSync(`./public/img/${req.file.filename}`)
+                }
+                error(res, 400, 'Nothing Patched, Wrong ID', {}, {})
+              }
+            })
+            .catch((err) => {
+              if (req.file) {
+                fs.unlinkSync(`./public/img/${req.file.filename}`)
+              }
+              error(res, 400, 'Wrong Data Type Given', err.message, {})
+            })
+        }
+      } else {
+        error(res, 400, 'Nothing Patched, No Data Given', 'Empty Data', {})
+      }
+    } catch (err) {
+      if (req.file) {
+        fs.unlinkSync(`./public/img/${req.file.filename}`)
+      }
+      error(res, 500, 'Internal Server Error', err, {})
+    }
+  },
+  cActivate: async (req, res) => {
     try {
       const token = req.params.token
       const currDate = moment().format('YYYY-MM-DDThh:mm:ss.ms');
       await jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if(err){
+        if (err) {
           error(res, 400, 'Invalid Token', err.message, {})
-        }else{
+        } else {
           mDetail(decoded.id)
             .then((responseDetail) => {
               if (responseDetail[0].active === 1) {
-                success(res, 200, 'User dont need any activation.', {},  {})
+                success(res, 200, 'User dont need any activation.', {}, {})
               } else {
-                mUpdateUser({active: 1, 'updated_at': currDate}, decoded.id)
+                mUpdateUser({
+                    active: 1,
+                    'updated_at': currDate
+                  }, decoded.id)
                   .then((response) => {
                     if (response.affectedRows != 0) {
                       success(res, 200, 'User Activated, You can login now!', {}, {})
@@ -137,45 +243,47 @@ module.exports = {
       error(res, 500, 'Internal Server Error', err.message, {})
     }
   },
-  cForgetToken: async(req, res) => {
+  cForgetToken: async (req, res) => {
     const body = req.body
     mCheckEmail(body.email)
-    .then(async (response)=>{
-      if(response.length === 1){
-        const dataUser = {
-          email: body.email,
-          id: response[0].id
-        }
-        const jwttoken = jwt.sign(dataUser, process.env.JWT_SECRET)
-        const link = `${process.env.IP_SERVER}:${process.env.PORT}/api/reset?token=${jwttoken}`
-        const mailData = `
+      .then(async (response) => {
+        if (response.length === 1) {
+          const dataUser = {
+            email: body.email,
+            id: response[0].id
+          }
+          const jwttoken = jwt.sign(dataUser, process.env.JWT_SECRET)
+          const link = `${process.env.IP_SERVER}:${process.env.PORT}/api/reset?token=${jwttoken}`
+          const mailData = `
         <div style="text-align:center">
           <h1>Click To Reset Your Account</h1>
           <p> Click the link below to Reset you account</p>
           <a href="${link}">Reset your account</a>
         </div>
         `
-        mSendMail(body.email, "Reset Account", mailData)
-          .then((resVerif) => {
-            success(res, 200, 'Reset Account Success, Check your email to reset your account!', {})
-          })
-          .catch((err) => error(res, 400, 'Reset Account Failed', err.message))
-      }else{
-        error(res, 400, 'Reset Password Failed', 'Email is not registered')
-      }
-    })
-    .catch((err)=> error(res, 400, 'Reset Password Failed', err.message))
+          mSendMail(body.email, "Reset Account", mailData)
+            .then((resVerif) => {
+              success(res, 200, 'Reset Account Success, Check your email to reset your account!', {})
+            })
+            .catch((err) => error(res, 400, 'Reset Account Failed', err.message))
+        } else {
+          error(res, 400, 'Reset Password Failed', 'Email is not registered')
+        }
+      })
+      .catch((err) => error(res, 400, 'Reset Password Failed', err.message))
   },
-  cForgetVerify: async(req, res) => {
+  cForgetVerify: async (req, res) => {
     try {
       const token = req.query.token
       await jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if(err){
+        if (err) {
           error(res, 400, 'Invalid Token', err.message, {})
-        }else{
+        } else {
           mDetail(decoded.id)
             .then((responseDetail) => {
-                success(res, 200, 'Token Verified!', {}, {id: responseDetail[0].id})
+              success(res, 200, 'Token Verified!', {}, {
+                id: responseDetail[0].id
+              })
             })
             .catch((err) => {
               error(res, 500, 'Internal Server Error', err.message, {})
--- res/routes/users.js
+++ res/routes/users.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const { singleUpload } = require('../helpers/upload')
-const { cRegister, cLogin, cDetail, cActivate, cForgetToken, cForgetVerify} = require('../controllers/users')
+const { cRegister, cLogin, cDetail, cActivate, cForgetToken, cForgetVerify, cUpdateUser} = require('../controllers/users')
 
 const route = express.Router()
 route
 .post('/api/register', cRegister)
 .post('/api/login', cLogin)
-.get('/api/user/:id', cDetail)
-.get('/api/verify/:token', cActivate )
 .post('/api/reset', cForgetToken )
 .get('/api/reset', cForgetVerify )
+.get('/api/verify/:token', cActivate )
+.get('/api/user/:id', cDetail)
+.patch('/api/user/:id', singleUpload, cUpdateUser)
   // .patch('/api/user/:id', authentication, singleUpload, patchUser)
 
 module.exports = route
